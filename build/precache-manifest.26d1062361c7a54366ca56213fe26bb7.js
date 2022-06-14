baa0a7ac1e492647000de7f07535747fb8ceb478
josephfernicola <josephrfernicola@gmail.com>
2022-06-13 21:03:15 -0700

    changed logo and name

---
+++ build/precache-manifest.26d1062361c7a54366ca56213fe26bb7.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = [
+  {
+    "revision": "d35dc502116c2faae282",
+    "url": "/memory-card/static/css/main.a0cb7312.chunk.css"
+  },
+  {
+    "revision": "d35dc502116c2faae282",
+    "url": "/memory-card/static/js/main.2b19cbc4.chunk.js"
+  },
+  {
+    "revision": "115cb7ee45d13c863cd8",
+    "url": "/memory-card/static/js/runtime~main.16318a33.js"
+  },
+  {
+    "revision": "da941e2bf06d5557d584",
+    "url": "/memory-card/static/js/2.8e5bdfda.chunk.js"
+  },
+  {
+    "revision": "bb895545e484ff0164d4",
+    "url": "/memory-card/static/js/3.71a42320.chunk.js"
+  },
+  {
+    "revision": "9ee66175084771e8d2475639327df856",
+    "url": "/memory-card/static/media/strokes.9ee66175.webp"
+  },
+  {
+    "revision": "364f81152562de1cb43fcb2a95d3c744",
+    "url": "/memory-card/static/media/modestMouse.364f8115.jpg"
+  },
+  {
+    "revision": "76fa5652cf286696c9870b0773175306",
+    "url": "/memory-card/static/media/radiohead.76fa5652.jpg"
+  },
+  {
+    "revision": "4ef8a909595ffe2cea8edf4729dad8b7",
+    "url": "/memory-card/static/media/velvetUnderground.4ef8a909.jpeg"
+  },
+  {
+    "revision": "292b809b608973815c8024bac96801c9",
+    "url": "/memory-card/static/media/bonIver.292b809b.jpeg"
+  },
+  {
+    "revision": "1b92cc5d1752881cc9702a0a721e6b2b",
+    "url": "/memory-card/static/media/talkingHeads.1b92cc5d.webp"
+  },
+  {
+    "revision": "0e99cdaf3f2f06578ab04c1324015382",
+    "url": "/memory-card/static/media/freddie.0e99cdaf.jpeg"
+  },
+  {
+    "revision": "9d4ef80b3e124b213c2b9c0f22f42310",
+    "url": "/memory-card/static/media/lcd.9d4ef80b.jpeg"
+  },
+  {
+    "revision": "3e06667245b2c8261cb247ccbf88c164",
+    "url": "/memory-card/static/media/taylor.3e066672.png"
+  },
+  {
+    "revision": "d0d9e467373d9df845c83ae7f49f0015",
+    "url": "/memory-card/static/media/kasey.d0d9e467.webp"
+  },
+  {
+    "revision": "d41cedfcd6b5c60c6000bb3637dc1626",
+    "url": "/memory-card/static/media/newOrder.d41cedfc.png"
+  },
+  {
+    "revision": "76e1d18d911b3c60cc5890b351689936",
+    "url": "/memory-card/static/media/sufjan.76e1d18d.jpeg"
+  },
+  {
+    "revision": "19fbf93c472ae9f001857edfb54ab3aa",
+    "url": "/memory-card/index.html"
+  }
+];
